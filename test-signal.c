bc99f180a143b1096db0496a18c483b0084191b4
Seppe Stas <seppestas@gmail.com>
2016-08-08 11:36:02 +0200

    Fixed various stuff in Unity panel entry signal handling

--- test-signal.c
+++ test-signal.c
@@ -20,18 +20,56 @@ void workaround_subsribe_about_to_show(AppIndicator *indicator)
     g_signal_connect(rootMenuItem, "about-to-show", G_CALLBACK(on_about_to_show), NULL);
 }
 
-struct panel_entry {
+typedef struct panel_entry_s {
     gchar* id;
     void (*on_activate)(gint x, gint y, guint w, guint h);
-};
+} PanelEntry;
+
+
+void handle_resync(GDBusProxy *proxy, PanelEntry *panel_entry)
+{
+    GError *err = NULL;
+    GVariant *appindicators = g_dbus_proxy_call_sync(
+        proxy,
+        "SyncOne",
+        g_variant_new("(s)", "libapplication.so"),
+        G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        NULL,
+        &err
+    );
+
+    if (err != NULL)
+    {
+        printf("Error syncing libapplication: %s\n", err->message);
+        g_error_free(err);
+        return;
+    }
+
+    GVariantIter *iter;
+    gchar *signature;
+    gchar *id;
+    gchar *entry_name;
+    g_variant_get(appindicators, "(a(sssusbbusbbi))", &iter);
+    while (g_variant_iter_loop(iter, "(sssusbbusbbi)", &signature, &id, &entry_name, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
+    {
+        if (g_strcmp0(signature, "libapplication.so") == 0 && g_strcmp0(entry_name, "testing-123") == 0)
+        {
+            if (g_strcmp0(panel_entry->id, id) != 0)
+            {
+                if (panel_entry->id != NULL)
+                    g_free(panel_entry->id);
+                panel_entry->id = g_strdup(id);
+            }
+        }
+    }
+    g_variant_iter_free(iter);
+    g_variant_unref(appindicators);
+}
 
 void on_unity_panel_signal(GDBusProxy *proxy, gchar *sender_name, gchar *signal_name, GVariant *parameters, gpointer user_data)
 {
-    struct panel_entry *entry = (struct panel_entry *) user_data;
-
-    gchar *parameters_str = g_variant_print (parameters, TRUE);
-    printf("Signal: %s, params: %s\n", signal_name, parameters_str);
-    g_free(parameters_str);
+    PanelEntry *panel_entry = (PanelEntry *) user_data;
 
     if (g_strcmp0(signal_name, "ReSync") == 0)
     {
@@ -39,71 +77,23 @@ void on_unity_panel_signal(GDBusProxy *proxy, gchar *sender_name, gchar *signal_
         g_variant_get(parameters, "(s)", &to_sync);
         if (g_strcmp0(to_sync, "libapplication.so") == 0)
         {
-            printf("Time to sync %s\n", to_sync);
-            GError *err = NULL;
-            GVariant *appindicators = g_dbus_proxy_call_sync(
-                proxy,
-                "SyncOne",
-                g_variant_new("(s)", "libapplication.so"),
-                G_DBUS_CALL_FLAGS_NONE,
-                -1,
-                NULL,
-                &err
-            );
-
-            if (err != NULL)
-            {
-                printf("Error syncing libapplication: %s\n", err->message);
-                g_error_free(err);
-                return;
-            }
-
-            GVariantIter *iter;
-            gchar *signature;
-            gchar *id;
-            gchar *entry_name;
-            g_variant_get(appindicators, "(a(sssusbbusbbi))", &iter);
-            while (g_variant_iter_loop(iter, "(sssusbbusbbi)", &signature, &id, &entry_name, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
-            {
-                if (g_strcmp0(signature, "libapplication.so") == 0 && g_strcmp0(entry_name, "testing-123") == 0)
-                {
-                    printf("Entry id: %s\n", id);
-                    if (g_strcmp0(entry->id, id) != 0)
-                    {
-                        // if (entry->id != NULL)
-                        //     g_free(entry->id);
-                        entry->id = g_strdup(id);
-                    }
-                }
-            }
-            g_variant_iter_free(iter);
+            handle_resync(proxy, panel_entry);
         }
         g_free(to_sync);
-        
-        // Check if it is for "libapplication.so"
-        // Send SyncOne method call
-        // change id of indicator
     }
     else if (g_strcmp0(signal_name, "EntryActivated") == 0)
     {
-        gchar* panel_id;
         gchar* entry_id;
         gint x, y;
         guint w, h;
-        g_variant_get(parameters, "(ss(iiuu))", &panel_id, &entry_id, &x, &y, &w, &h);
-        printf("Entry activates! Panel: %s, entry id: %s\n", panel_id, entry_id);
-        printf("Entry id: %s\n", entry->id);
-        if (g_strcmp0(panel_id, entry->id) == 0)
+        g_variant_get(parameters, "(ss(iiuu))", NULL, &entry_id, &x, &y, &w, &h);
+
+        if (g_strcmp0(panel_entry->id, entry_id) == 0)
         {
-            printf("Yeey\n");
-            entry->on_activate(x, y, w, h);
+            panel_entry->on_activate(x, y, w, h);
         }
-        printf("Differnece: %d\n", g_strcmp0(panel_id, entry->id));
-        g_free(panel_id);
+
         g_free(entry_id);
-        // Do we have the id of the indicator
-        // is it equal to the one in of parameters?
-        // report click and location
     }
 
 }
@@ -123,21 +113,33 @@ void unity_panel_proxy_ready(GObject *source_object, GAsyncResult *res, gpointer
     g_signal_connect(unity_panel_proxy, "g-signal", G_CALLBACK(on_unity_panel_signal), user_data);
 }
 
-void on_panel_activate(gint x, gint y, guint w, guint h)
+static void on_panel_activate(gint x, gint y, guint w, guint h)
 {
     printf("Panel activated! x: %d, y: %d, w: %d, h: %d\n", x, y, w, h);
 }
+
+PanelEntry *panel_entry_new(void (*on_activate)(gint x, gint y, guint w, guint h))
+{
+    PanelEntry *entry = g_malloc(sizeof(PanelEntry));
+    entry->id= NULL;
+    entry->on_activate = on_activate;
+}
+
+void panel_entry_free(PanelEntry *entry)
+{
+    printf("Freeing panel entry\n");
+    if (entry->id != NULL)
+        g_free(entry->id);
+    g_free(entry);
+}
+
 
 // INCOMPLETE
 // Detect clicks by subscribing to "EntryActivated" signals from the Unity
 // panel service. This also contains the location of the icon.
 // https://bugs.launchpad.net/ubuntu/+source/libappindicator/+bug/522152/comments/11
-void workaround_entry_activated()
+void workaround_entry_activated(PanelEntry* panel_entry)
 {
-    struct panel_entry entry = {
-        NULL,
-        &on_panel_activate
-    };
     g_dbus_proxy_new_for_bus(
         G_BUS_TYPE_SESSION,
         G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES | G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START,
@@ -147,7 +149,7 @@ void workaround_entry_activated()
         "com.canonical.Unity.Panel.Service",
         NULL,
         unity_panel_proxy_ready,
-        &entry
+        panel_entry
     );
 }
 
@@ -173,17 +175,16 @@ int main( int   argc,
 
     printf("Icon pointer: %p\n", indicator);
 
-    app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ATTENTION);
+    app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
 
     GtkWidget *menu_widget = gtk_menu_new();
     GtkMenu* menu = GTK_MENU(menu_widget);
     GtkMenuShell* menu_shell = GTK_MENU_SHELL(menu);
 
-    GtkWidget *menu_item = gtk_menu_item_new_with_mnemonic("hello, world");
-
+    GtkWidget *menu_item = gtk_menu_item_new_with_mnemonic("");
     gtk_widget_show(menu_item);
     gtk_menu_shell_append(menu_shell, menu_item);
-    
+
     app_indicator_set_menu(indicator, menu);
 
     // GTkStatusIcon test
@@ -192,8 +193,11 @@ int main( int   argc,
 
     workaround_subsribe_about_to_show(indicator);
 
-    workaround_entry_activated();
+    PanelEntry *panel_entry = panel_entry_new(&on_panel_activate);
+    workaround_entry_activated(panel_entry);
 
     gtk_main();
+
+    panel_entry_free(panel_entry);
     return 0;
 }
